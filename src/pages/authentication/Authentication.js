7c39f63808cfaa4797bc6cf3ab8fc50e4bb14cb8
Nguyen Pham <nguyenpham@Nguyens-MacBook-Pro.local>
2018-03-06 08:43:30 +0700

    make change for app flow update company url input

--- src/pages/authentication/Authentication.js
+++ src/pages/authentication/Authentication.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, WebView } from 'react-native'
+import { Container, Header, Content, Form, Item, Input, Label, Button, Text } from 'native-base'
 
 export default class Authentication extends Component {
 
@@ -7,11 +8,10 @@ export default class Authentication extends Component {
     super(props)
     const TAG = 'AUTHENTICATION'
     this.state = {
-      url: 'http://' + this.props.navigation.state.params.domainName + '/web#login',
-      domainName: this.props.navigation.state.params.domainName,
-      username: this.props.navigation.state.params.username,
-      password: this.props.navigation.state.params.password,
-      session_id: this.props.navigation.state.params.session_id
+      url: 'about:blank',
+      // url: 'http://' + this.props.navigation.state.params.domainName + '/web#login',
+      domainName: '',
+      session_id: ''
     }
   }  
   
@@ -43,19 +43,38 @@ export default class Authentication extends Component {
     let jsCode = `document.cookie = 'session_id='` + this.state.session_id + `";`
     return (
       <View>
+        <Content style={styles.container}>
+          <Image source={require('../../../images/w360s-logo.jpg')}
+              style={styles.imageLogo}
+              resizeMode='contain'
+            />
+          <Form style={styles.formWrapper}>
+              <Item floatingLabel error={this.state.domainNameError} style={styles.inputTextWrapper}>
+                <Label>Company Domain Name</Label>
+                <Input autoCapitalize={'none'} style={styles.inputText}
+                    autoCorrect={false}
+                    onBlur={(domainName) => this.checkDomainName(domainName)}
+                    onChangeText={(domainName) => this.setState({domainName})}
+                    value={this.state.domainName}/>
+              </Item>
+          </Form>
+        </Content>
         <WebView
           source={{uri: this.state.url}}
           javaScriptEnabled={true}
           injectedJavaScript={jsCode}
           onNavigationStateChange={ this.onNavigationStateChange.bind(this) }
           startInLoadingState={true}
-          style={{
-            marginTop: 20
-          }}/>
+          style={styles.hiddenWebView}/>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  hiddenWebView: {
+    width: 0,
+    height: 0,
+    marginTop: 20
+  }
 });
