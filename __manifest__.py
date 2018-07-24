96bb855ab43246302c61be52e142e4033529675d
Chau Le <chaulb@icloud.com>
2018-07-24 14:53:14 +0700

    update
    update pricing

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'license': 'LGPL-3',
+    "name" : "Odoo Mobile App buider",
+    "version" : "1.0",
+    "author" : "W360S",
+    'price': 299.90,
+    'currency': 'EUR',
+    "category" : "Sales",
+	"summary": "Personalize your Own Odoo Mobile App with odoo app builer!",
+    'description': "Odoo is the greatest web app system for enterprise but doesn't have app for community version, besides on that W360S Team we build on react native that can help you to generate your own Odoo system on mobile for both android & ios. So now you can have your own mobile apps, just need Odoo CE + Odoo backend theme and with Odoo mobile app builder you can get your own system with your own brand name.",
+    'maintainer': "World 360° Services",
+    'website': 'https://www.saastools.xyz',
+    'images': ['static/description/banner.png'],
+    "depends" : ["base"],
+    "auto_install": False,
+    "application": False,
+    "installable": True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
